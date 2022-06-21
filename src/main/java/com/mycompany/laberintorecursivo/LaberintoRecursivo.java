e7412a33ed12f5f1ea6b042f95e5eb3814fa40bd
LAMF14 <65782418+LAMF14@users.noreply.github.com>
2022-06-20 17:55:20 -0700

    Final

---
+++ src/main/java/com/mycompany/laberintorecursivo/LaberintoRecursivo.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.laberintorecursivo;
+
+/**
+ *
+ * @author luisa
+ */
+public class LaberintoRecursivo {
+    
+    static char[][] laberintoPrincipal =
+            {
+                    {'#','#','#','#','#','#','#','#','#','#','#','#'},
+                    {'#',' ', '#','#','#','#','#','#','#','#','#','#'},
+                    {'#',' ', ' ','#','#',' ',' ',' ',' ',' ','#','#'},
+                    {'#','#', ' ','#','#',' ','#','#','#',' ','#','#'},
+                    {'#','#', ' ','#','#',' ',' ',' ','#',' ','#','#'},
+                    {'#','#', ' ','#','#',' ','#','#','#',' ','#','#'},
+                    {'#','#', ' ',' ','#',' ','#',' ','#',' ','#','#'},
+                    {'#','#', ' ','#','#',' ','#',' ','#',' ','#','#'},
+                    {'#','#', ' ','#','#',' ',' ',' ','#',' ','#','#'},
+                    {'#','#', ' ',' ',' ',' ','#','#','#',' ','#','#'},
+                    {'#','#', '#','#','#','#','#','#','#',' ',' ','F'},
+                    {'#','#','#','#','#','#','#','#','#','#','#','#'}};
+
+    public static void main(String[] args) {
+        
+         Laberinto laberinto = new Laberinto(laberintoPrincipal);
+        if(laberinto.resolucionLaberinto(1,1)){
+            System.out.println(" ");
+            System.out.println("FELICIDADES, LABERINTO RESUELTO.");
+        }else{
+            System.out.println(" ");
+            System.out.println("QUE LASTIMA, EL LABERINTO NO ES POSIBLE.");
+        }
+    }
+}
